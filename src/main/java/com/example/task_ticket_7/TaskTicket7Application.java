644e039bb71f34a7f74bfe10ec60bcea2ebc4fa8
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2023-02-18 18:53:27 +0300

    Initial commit

---
+++ src/main/java/com/example/task_ticket_7/TaskTicket7Application.java
@@ -0,0 +1,13 @@
+package com.example.task_ticket_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTicket7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTicket7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task_ticket_7/TaskTicket7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_ticket_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTicket7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
